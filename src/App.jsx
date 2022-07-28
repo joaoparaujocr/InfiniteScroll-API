2b73bcb977bc3158581f20732c30947dcc43b0e6
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2022-07-28 09:13:51 -0400

    fix: render next and previous

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import api from './services/api';
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
   const [listCharacter, setListCharacter] = useState([]);
+  const [numberNextPage, setNumberNextPage]  = useState(currentPage + 1);
   const [maxPages, setMaxPages] = useState(0);
 
   useEffect(() => {
@@ -17,10 +18,33 @@ function App() {
     }).then(res => {
       if (res.status >= 200 && res.status <= 300) {
         setMaxPages(res.data.info.pages);
-        setListCharacter(res.data.results);
+        /* setListCharacter(arr => [...arr, ...res.data.results]); */
+        setListCharacter(arr => {
+          if (arr.length > 0) {
+            const newArr = res.data.results.filter(({ id }) => arr.some(({ id: idResults }) => id !== idResults))
+            const arrFilter = arr.filter(({ id }) => !newArr.some(({ id: idNewmArray }) => id === idNewmArray))
+            return [...arrFilter, ...newArr]
+          }
+
+          return res.data.results
+        });
+        setNumberNextPage(currentPage + 1);
       }
     })
   }, [currentPage]);
+
+  useEffect(() => {
+    api.get("character", {
+      params: {
+        page: numberNextPage
+      }
+    }).then(res => {
+      if (res.status >= 200 && res.status <= 300) {
+        const listNext = res.data.results;
+        setListCharacter(arr => arr.filter(({ id }) => !listNext.some(({ id: idResults }) => id === idResults)));
+      }
+    })
+  }, [numberNextPage]);
 
   return (
     <div className="App">
