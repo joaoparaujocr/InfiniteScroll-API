6daa25ebad6568a9c6f732337f8a91f72970602e
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2022-08-26 15:48:11 -0400

    acroll infinite

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,48 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import Buttons from './components/Buttons';
 import ListCard from './components/ListCard';
 import api from './services/api';
 
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
   const [listCharacter, setListCharacter] = useState([]);
-  const [numberNextPage, setNumberNextPage]  = useState(currentPage + 1);
-  const [maxPages, setMaxPages] = useState(0);
+  const [maxPages, setMaxPages] = useState(1);
 
-  useEffect(() => {
+  const loadCharacters = () => {
     api.get("character", {
       params: {
         page: currentPage
       }
-    }).then(res => {
-      if (res.status >= 200 && res.status <= 300) {
-        setMaxPages(res.data.info.pages);
-        /* setListCharacter(arr => [...arr, ...res.data.results]); */
-        setListCharacter(arr => {
-          if (arr.length > 0) {
-            const newArr = res.data.results.filter(({ id }) => arr.some(({ id: idResults }) => id !== idResults))
-            const arrFilter = arr.filter(({ id }) => !newArr.some(({ id: idNewmArray }) => id === idNewmArray))
-            return [...arrFilter, ...newArr]
-          }
-
-          return res.data.results
-        });
-        setNumberNextPage(currentPage + 1);
-      }
     })
-  }, [currentPage]);
+    .then(({ data }) => {
+      setListCharacter(listPrevious => [...listPrevious, ...data.results])
+      setMaxPages(data.info.pages)
+    })
+    .catch(err => console.log("pagina acabou"))
+  }
+
+  const handleScroll = e => {
+    console.log("top: ", e.target.documentElement.scrollTop);
+    console.log("window: ", window.innerHeight)
+    console.log("heigth: ", e.target.documentElement.scrollHeight)
+
+    if (window.innerHeight + e.target.documentElement.scrollTop >= e.target.documentElement.scrollHeight) {
+      setCurrentPage(pageActual => pageActual + 1);
+    }
+  } 
 
   useEffect(() => {
-    api.get("character", {
-      params: {
-        page: numberNextPage
-      }
-    }).then(res => {
-      if (res.status >= 200 && res.status <= 300) {
-        const listNext = res.data.results;
-        setListCharacter(arr => arr.filter(({ id }) => !listNext.some(({ id: idResults }) => id === idResults)));
-      }
-    })
-  }, [numberNextPage]);
+    if (!(currentPage > maxPages)) loadCharacters()
+    
+    window.addEventListener("scroll", handleScroll)
+  }, [currentPage]);
+
+
 
   return (
     <div className="App">
       <h1>Meus Personagens</h1>
       <ListCard listCharacter={listCharacter} />
-      <Buttons setCurrentPage={setCurrentPage} maxPages={maxPages} currentPage={currentPage} />
     </div>
   );
 }
